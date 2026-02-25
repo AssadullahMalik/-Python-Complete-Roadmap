b991c6f9a206198fa521cc5928d3284557d8b0a6
AssadullahMalik <asadullahmk102@gmail.com>
2026-02-25 14:53:41 +0500

    Update 08_Recursion.py

--- 08_Recursion.py
+++ 08_Recursion.py
@@ -3,4 +3,23 @@ def factorial(n):
         return 1
     return n * factorial(n - 1)
 
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+    
+    def print_reverse(n):
+    if n == 0:
+        return
+    print(n)
+    print_reverse(n - 1)
+
+
+print_reverse(5)
 print(factorial(5))
+print(fibonacci(6))  
+

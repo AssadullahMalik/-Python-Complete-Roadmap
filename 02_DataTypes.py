4af1d1bd55ada0bd9ae5bf553279124604c0de28
AssadullahMalik <asadullahmk102@gmail.com>
2026-02-25 14:48:10 +0500

    Update 02_DataTypes.py

---
+++ 02_DataTypes.py
@@ -0,0 +1,11 @@
+integer_value = 10
+float_value = 10.5
+string_value = "Python AI"
+boolean_value = True
+none_value = None
+
+print(type(integer_value))
+print(type(float_value))
+print(type(string_value))
+print(type(boolean_value))
+print(type(none_value))
